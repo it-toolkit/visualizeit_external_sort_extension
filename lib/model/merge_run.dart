3d02f4c68043da25a32f456c4ee80c26564a884a
Osiris <osirisarguello@gmail.com>
2024-05-29 15:57:06 -0300

    Agregados tests y corregidos errores en Merge Run

--- lib/model/merge_run.dart
+++ lib/model/merge_run.dart
@@ -1,9 +1,10 @@
 import 'package:collection/collection.dart';
+import 'package:visualizeit_extensions/logging.dart';
 
 class MergeRun<T extends Comparable<T>> {
 
-  
   final List<List<T>> _fragmentsToMerge;
+  Logger logger = Logger("extension.externalsort.mergerun");
 
   MergeRun(this._fragmentsToMerge);
 
@@ -15,7 +16,7 @@ class MergeRun<T extends Comparable<T>> {
     // Initialize the priority queue with the first element of each run.
     for (int i = 0; i < _fragmentsToMerge.length; i++) {
       if (_fragmentsToMerge[i].isNotEmpty) {
-        priorityQueue.add(_QueueEntry(_fragmentsToMerge[i][0], i, 0));
+        priorityQueue.add(_QueueEntry<T>(_fragmentsToMerge[i][0], i, 0));
       }
     }
 
@@ -28,21 +29,26 @@ class MergeRun<T extends Comparable<T>> {
       // Get the next element from the same run, if it exists.
       final nextIndex = smallestEntry.currentKeyPointer + 1;
       if (nextIndex < _fragmentsToMerge[smallestEntry.fragmentIndex].length) {
-        priorityQueue.add(_QueueEntry(
+        priorityQueue.add(_QueueEntry<T>(
             _fragmentsToMerge[smallestEntry.fragmentIndex][nextIndex],
             smallestEntry.fragmentIndex,
             nextIndex));
       }
     }
-
+    logger.debug(() => "Run result: $result");
     return result;
   }
 }
 
-class _QueueEntry<T extends Comparable<T>> {
+class _QueueEntry<T extends Comparable<T>> implements Comparable<_QueueEntry<T>>{
   final T value;
   final int fragmentIndex;
   final int currentKeyPointer;
 
   _QueueEntry(this.value, this.fragmentIndex, this.currentKeyPointer);
+  
+  @override
+  int compareTo(_QueueEntry<T> other) {
+    return value.compareTo(other.value);
+  }
 }
---
+++ test/merge_run_test.dart
@@ -0,0 +1,60 @@
+import 'package:collection/collection.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:visualizeit_external_sort_extension/model/merge_run.dart';
+
+void main() {
+  test("merge run", () {
+    List<num> fragment1 = [
+      410,
+      425,
+      427,
+      434,
+      446,
+      453,
+      466,
+      656,
+      717,
+      738,
+      973
+    ];
+    List<num> fragment2 = [
+      221,
+      264,
+      477,
+      486,
+      497,
+      503,
+      508,
+      514,
+      515,
+      529,
+      538,
+      552,
+      985
+    ];
+    List<num> fragment3 = [62, 144, 202, 220, 414, 481];
+
+    var mergeRun = MergeRun<num>([fragment1, fragment2, fragment3]);
+
+    var fragmentMerged = mergeRun.merge();
+    expect(fragmentMerged.isSorted(), true);
+    expect(fragmentMerged,
+        hasLength(fragment1.length + fragment2.length + fragment3.length));
+    expect(fragmentMerged, containsAll(fragment1));
+    expect(fragmentMerged, containsAll(fragment2));
+    expect(fragmentMerged, containsAll(fragment3));
+  });
+
+  test("merge with only one fragment", () {
+    List<num> fragment1 = [410, 425, 427, 434, 446, 453];
+
+    var mergeRun = MergeRun<num>([fragment1]);
+
+    var fragmentMerged = mergeRun.merge();
+
+    expect(fragmentMerged.isSorted(), true);
+    expect(fragmentMerged,
+        hasLength(fragment1.length));
+    expect(fragmentMerged, containsAll(fragment1));
+  });
+}
