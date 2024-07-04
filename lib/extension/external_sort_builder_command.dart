d9e5f23bc33e7b4b8e2c4ecd175160c829edd3bf
Osiris <osirisarguello@gmail.com>
2024-07-04 12:14:46 -0300

    Cambios en la validacion de los parametros

--- lib/extension/external_sort_builder_command.dart
+++ lib/extension/external_sort_builder_command.dart
@@ -11,26 +11,36 @@ class ExternalSortBuilderCommand extends ModelBuilderCommand {
     CommandArgDef("fileToSort", ArgType.intArray)
   ]);
 
-  final List<int> fileToSort;
-  final int bufferSize;
-  final int fragmentLimit;
+  late List<int> fileToSort;
+  late int bufferSize;
+  late int fragmentLimit;
 
   ExternalSortBuilderCommand(
       this.bufferSize, this.fragmentLimit, this.fileToSort);
-  ExternalSortBuilderCommand.build(RawCommand rawCommand)
-      : bufferSize = _getIntArgInRange(name: "bufferSize", from: rawCommand, min: 2, max: 30),
-        fragmentLimit =  _getIntArgInRange(name: "fragmentLimit", from: rawCommand, min: 2, max: 30),
-        fileToSort = (commandDefinition.getArg(name: "fileToSort", from: rawCommand) as List<int>);
+  ExternalSortBuilderCommand.build(RawCommand rawCommand){
+    bufferSize = _getIntArgInRange(name: "bufferSize", rawCommand: rawCommand, min: 2, max: 15);
+    fragmentLimit = _getIntArgInRange(name: "fragmentLimit", rawCommand: rawCommand, min: 2, max: 10);
+    fileToSort = _validate(name: "fileToSort", rawCommand: rawCommand, bufferSize: bufferSize);
+  }
 
   @override
   ExternalSortModel call(CommandContext context) {
     return ExternalSortModel("", bufferSize, fragmentLimit, fileToSort);
   }
 
-  static _getIntArgInRange({required String name, required RawCommand from, required int min, required int max}) {
-    int value = commandDefinition.getArg(name: name, from: from);
+  static _getIntArgInRange({required String name, required RawCommand rawCommand, required int min, required int max}) {
+    int value = commandDefinition.getArg(name: name, from: rawCommand);
     if (value < min || value > max) throw Exception("Value must be in range [ $min , $max ]");
 
     return value;
+  }
+  
+  static _validate({required String name, required RawCommand rawCommand, required int bufferSize}) {
+    var fileToSort = commandDefinition.getArg(name: "fileToSort", from: rawCommand) as List<int>;
+
+    if(fileToSort.length <= bufferSize){
+      throw Exception("File must have more keys than buffer size");
+    }
+    return fileToSort;
   }
 }
--- test/extension/external_sort_builder_command_test.dart
+++ test/extension/external_sort_builder_command_test.dart
@@ -1,25 +1,70 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:visualizeit_extensions/common.dart';
+import 'package:visualizeit_extensions/scripting.dart';
 import 'package:visualizeit_external_sort_extension/extension/external_sort_builder_command.dart';
 import 'package:visualizeit_external_sort_extension/extension/external_sort_extension.dart';
 
 class CommandContextMock extends Mock implements CommandContext {}
 
-void main(){
+void main() {
   var commandContextMock = CommandContextMock();
 
-   test("Call test", () {
+  test("Call test", () {
     var command = ExternalSortBuilderCommand(3, 5, [1, 2, 3, 4]);
 
     var model = command.call(commandContextMock);
     expect(model.extensionId, ExternalSortExtension.extensionId);
-    expect(model.name,
-        ""); //TODO arreglar este test cuando entienda que es el name
+    expect(model.name, "");
     expect(model.bufferSize, 3);
     expect(model.fragmentLimit, 5);
     expect(model.fileToSort, containsAll([1, 2, 3, 4]));
     expect(model.commandInExecution, isNull);
     expect(model.currentTransition, isNull);
+  });
+
+  test("Fragment limit Error less than min", () {
+    var rawCommand = RawCommand.withPositionalArgs("externalsort-create", [
+      3,
+      1,
+      ["1", "2", "3", "4"]
+    ]);
+
+    expect(
+        () => ExternalSortBuilderCommand.build(rawCommand),
+        throwsA(allOf(
+            isException,
+            predicate(
+                (e) => e.toString().contains("Value must be in range")))));
+  });
+
+  test("Fragment limit Error more than max", () {
+    var rawCommand = RawCommand.withPositionalArgs("externalsort-create", [
+      3,
+      11,
+      ["1", "2", "3", "4"]
+    ]);
+
+    expect(
+        () => ExternalSortBuilderCommand.build(rawCommand),
+        throwsA(allOf(
+            isException,
+            predicate(
+                (e) => e.toString().contains("Value must be in range")))));
+  });
+
+  test("File to sort size must be greater than buffer size", () {
+    var rawCommand = RawCommand.withPositionalArgs("externalsort-create", [
+      5,
+      3,
+      ["1", "2", "3", "4"]
+    ]);
+
+    expect(
+        () => ExternalSortBuilderCommand.build(rawCommand),
+        throwsA(allOf(
+            isException,
+            predicate(
+                (e) => e.toString().contains("File must have more keys than buffer size")))));
   });
 }
